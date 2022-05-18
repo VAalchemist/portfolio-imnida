603910e210794562324f292ea658be248c7a2875
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-17 21:11:33 -0700

    work container shelled before content placed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,39 +1,51 @@
 import React from 'react';
 import One from '../assets/one.png';
-import Two from '../assets/two.png';
-import Three from '../assets/three.jpg';
-import Four from '../assets/four.jpg';
-import Five from '../assets/five.jpg';
-import Six from '../assets/six.png';
+// import Two from '../assets/two.png';
+// import Three from '../assets/three.jpg';
+// import Four from '../assets/four.jpg';
+// import Five from '../assets/five.jpg';
+// import Six from '../assets/six.png';
 
 
 const Portfolio = () => {
   return (
-    <div name="portfolio" className='w-full md:h-screen flex flex-col justify-center items-center bg-teal-900 sm:pt-28'>
-      <div className='flex flex-col justify-center items-center bg-teal-900 w-full mt-16'>
+    <div name="portfolio" className='w-full h-screen bg-teal-900'>
+      {/* container */}
 
-        <div>
-          <img src={One} alt="my werk stuff" style={{ width: '90px' }} />
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline text-[#fdd978]'>My Work</p>
+          <p className='py-6 text-[#ffc9dc]'>Check out some of the fun things I recently did</p>
         </div>
 
-        <div>
-          <img src={Two} alt="my werk stuff" style={{ width: '90px' }} />
-        </div>
-      
-        <div>
-          <img src={Three} alt="my werk stuff" style={{ width: '90px' }} />
-        </div>
-      
-        <div>
-          <img src={Four} alt="my werk stuff" style={{ width: '90px' }} />
-        </div>
-      
-        <div>
-          <img src={Five} alt="my werk stuff" style={{ width: '90px' }} />
-        </div>
-      
-        <div>
-          <img src={Six} alt="my werk stuff" style={{ width: '90px' }} />
+        {/* project/grid container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-2xl shadow-stone-400 group container rounded-md flex justify-center items-center mx-auto content-div'>
+            
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 group-hover:duration-500'>
+              <div>
+                
+                <span className='text-2xl font-bold text-[#ffc9dc] tracking-wider'>
+                  First Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-stone-400 text-cyan-700 font-bold text-lg'>Demo</button>
+                  </a>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-stone-400 text-cyan-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
         </div>
         
       </div>            
