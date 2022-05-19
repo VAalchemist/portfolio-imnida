9264d4dffe5a84ed26d91e5a4038452e5de7508e
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-19 15:55:11 -0700

    first card done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,17 +9,17 @@ import One from '../assets/one.png';
 
 const Portfolio = () => {
   return (
-    <div name="portfolio" className='w-full h-screen bg-teal-900 '>
+    <div name="portfolio" className='w-full md:h-screen bg-teal-900 '>
       
       {/* container start */}
       <div className='max-width-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         
-        <div className='pb-8'>
-          <p className='text-4xl font-bol inline border-b-4 border-cyan-600 text-gray-300'>
-            Work
+        <div className='mt-28 pb-8'>
+          <p className='text-5xl md:text-6xl font-bold inline text-[#fdd978]'>
+            My Work
           </p>
-          <p className='py-6'>
-            WorkWorkWorkWorkWork
+          <p className='py-6 text-[#ffc9dc]'>
+            Check out some of the fun things I recently did
           </p>
         </div>
 
@@ -27,23 +27,147 @@ const Portfolio = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
           {/* card item */}
-          <div className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* hover fx */}
-              <div>                
-                <span></span>
+              <div className='opacity-0 group-hover:opacity-100 group-hover:duration-500'>                
+                <span className='text-2xl font-bold text-[#8d767e] tracking-wider'>
+                  Redact JS Application
+                </span>
 
-                <div>
+                <div className='pt-8 text-center'>
                   <a href="/">
-                    <button>
-
-                    </button>
+                    <button className='text-center rounded-lg px-3 py-2 m-2 bg-stone-400 text-cyan-700 font-bold text-lg'>Demo</button>
                   </a>
 
                   <a href="/">
-                    <button>
-                      
-                    </button>
+                    <button className='text-center rounded-lg px-3 py-2 m-2 bg-stone-400 text-cyan-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+              </div>
+
+
+          </div>
+          {/* card item */}
+
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover fx */}
+              <div className='opacity-0 group-hover:opacity-100 '>                
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Redact JS Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+              </div>
+
+
+          </div>
+          {/* card item */}
+
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover fx */}
+              <div className='opacity-0 group-hover:opacity-100 '>                
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Redact JS Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+              </div>
+
+
+          </div>
+          {/* card item */}
+
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover fx */}
+              <div className='opacity-0 group-hover:opacity-100 '>                
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Redact JS Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+              </div>
+
+
+          </div>
+          {/* card item */}
+
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover fx */}
+              <div className='opacity-0 group-hover:opacity-100 '>                
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Redact JS Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                  </a>
+                </div>
+              </div>
+
+
+          </div>
+          {/* card item */}
+
+          {/* card item */}
+          <div style={{backgroundImage: `url(${One})`}}
+            className='shadow-lg shadow-stone-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* hover fx */}
+              <div className='opacity-0 group-hover:opacity-100 '>                
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Redact JS Application
+                </span>
+
+                <div className='pt-8 text-center'>
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+
+                  <a href="/">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                   </a>
                 </div>
               </div>
