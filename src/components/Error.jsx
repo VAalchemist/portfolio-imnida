fdbafdd6544ccf91f9e87f1ad71dee8b5fcba538
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-19 20:53:09 -0700

    error page redirect done

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { useNavigate } from 'react-router-dom';
 
 function Error() {
+  let navigate = useNavigate();
+
   return (
     <div className='w-full h-screen bg-teal-900 flex justify-between items-center px-4 font-bold text-7xl'>
       NOPE not here..
+
+      <button onClick={() => {
+        navigate("/");
+      }}
+        className='text-white group text-sm border-2 px-3 py-2 my-2 flex items-center rounded hover:bg-[#ecdbba] hover:border-[#a3a9ae] hover:text-stone-700 hover:font-bold'>Go Home
+            <span className='group-hover:rotate-180 duration-300'>
+              <HiArrowNarrowRight className='ml-2' />
+            </span>
+          </button>
     </div>
   )
 }
