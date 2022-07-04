e1b6d0885660a3babb5adba2ff69d2641a3fe37c
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-07-03 23:14:24 -0700

    white space padding fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,31 +8,19 @@ function About() {
   return (
 
 
-  <div name='about' className='w-full h-full sm:h-screen flex flex-col justify-center items-center bg-teal-900 pt-28'>
-    <div className='flex flex-col justify-center items-center w-full h-full'>
-    <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-
-    <div className='sm:text-right pb-6 md:pb-16 pl-4'p>
-      <h1 className='text-[#fdd978] text-4xl sm:text-6xl font-bold tracking-wide sm:inline mt-12'>About Me!</h1>        
-    </div>          
-          <div></div>         
-    </div>
-          <div className='max-w-[1000px] w-full px-4 grid sm:grid-cols-2 gap-2'>
-            
-            <div className='sm:text-right text-4xl font-bold'>
-              <img src={Me} alt="my face" className='rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl w-60 md:w-72' />
-            </div>
-
-          <div>
-            <p className='text-[#ffc9dc] text-center md:text-lg lg:text-xl py-2 '>From the eclectic array of interests I acquired  throughout the decades of my existence, full stack development has eagerly entered  my arsenal of skills. My professional path has taken me through many twists and turns, yet I've always sought challenges and opportunities that are meaningful to me. If I'm not tickling the ivory key caps, you can find me skillfully enjoying <Typed 
+  <div name='about' className='w-full h-full sm:h-screen bg-teal-900 pb-56'>
+      <div className='flex flex-col items-center w-full h-full md:pt-16 lg:pt-28'>
+        <div className='flex flex-col md:flex-row items-center mt-12'>
+          <h1 className='text-[#fdd978] py-6 md:pr-6 text-4xl sm:text-6xl font-bold tracking-wide sm:inline'>About Me!</h1> 
+        
+        <img src={Me} alt="my face" className='rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl w-60 md:pb-2 md:w-72' />
+        </div>        
+        <p className='text-[#ffc9dc] text-center md:text-lg lg:text-xl py-2 px-12 max-w-[1000px]'>From the eclectic array of interests I acquired  throughout the decades of my existence, full stack development has eagerly entered  my arsenal of skills. My professional path has taken me through many twists and turns, yet I've always sought challenges and opportunities that are meaningful to me. If I'm not tickling the ivory key caps, you can find me skillfully enjoying <Typed 
               className='md:text-3xl sm:text-2xl text-xl font-bold'
-              strings={['singing', 'voice acting', 'gaming', 'sewing', 'eating']}
+              strings={['singing.', 'voice acting.', 'gaming.', 'sewing.', 'eating.']}
               typeSpeed={120}
               backSpeed={140}
               loop /></p> 
-          </div>  
-          
-          </div>                     
     </div>     
   </div>
 
