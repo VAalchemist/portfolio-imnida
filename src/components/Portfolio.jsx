95ca185169d0a54af93764d6baf6dc49e19081e1
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-07-04 00:41:52 -0700

    card spacing fluidity, begun text for projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,62 +1,161 @@
 import React from 'react';
 import viper from '../assets/viper.jpg';
-import sage from '../assets/sage.png';
-import kj from '../assets/kj.jpg';
-
 
 
 const Portfolio = () => {
   return (
-    <div className='w-ful py-[10rem] px-4 bg-white'>
-    <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+    <div className='w-full h-full py-[10rem] pb-96 bg-teal-900'>
       
-      <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-        <img className='w-20 mx-auto mt-[-3rem] bg-white' src={viper} alt="/" />
-        <h2 className='text-2xl font-bold text-center py-8'>Viper main</h2>
-        <p className='text-center text-4xl font-bold'>$149</p>
+      {/* Card Content */}
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 mb-12'>
+        
+      {/* First Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>Forged Ensemble</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
 
-        <div className='text-center font-medium'>
+        <div className=' text-center font-medium'>
           <p className='py-2 border-b mx-8 mt-8'>500 GB storage</p>
-          <p className='py-2 border-b mx-8'>#1 main user</p>
-          <p className='py-2 border-b mx-8'>Send up to 2 mollies</p>
+            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              Mongoose ||
+              MongoDB</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
         </div>
 
-        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-black'>Start Trial</button>
-      </div>
+          <div className='flex justify-between'>
+            <a href='https://github.com/VAalchemist/forged-ensemble' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
+        
+      {/* Second Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>Duty Call</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
 
-      <div className='w-full shadow-xl bg-stone-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-        <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={sage} alt="/" />
-        <h2 className='text-2xl font-bold text-center py-8'>Sage main</h2>
-        <p className='text-center text-4xl font-bold'>$149</p>
-
-        <div className='text-center font-medium'>
+        <div className='text-center font-medium '>
           <p className='py-2 border-b mx-8 mt-8'>500 GB storage</p>
-          <p className='py-2 border-b mx-8'>#1 main user</p>
-          <p className='py-2 border-b mx-8'>Send up to 2 orbs</p>
+           <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              Mongoose ||
+              MongoDB</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
         </div>
 
-        <button className='text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 bg-black'>Start Trial</button>
-      </div>
+          <div className='flex justify-between'>
+            <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
       
+    {/* Third Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>KLEC Search</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
 
-      <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-        <img className='w-20 mx-auto mt-[-3rem] bg-white' src={kj} alt="/" />
-        <h2 className='text-2xl font-bold text-center py-8'>KJ main</h2>
-        <p className='text-center text-4xl font-bold'>$149</p>
-
-        <div className='text-center font-medium'>
+        <div className='text-center font-medium '>
           <p className='py-2 border-b mx-8 mt-8'>500 GB storage</p>
-          <p className='py-2 border-b mx-8'>#1 main user</p>
-          <p className='py-2 border-b mx-8'>Send up to 2 nanos</p>
+           <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              Mongoose ||
+              MongoDB</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
         </div>
 
-        <button className='text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 bg-black'>Start Trial</button>
+          <div className='flex justify-between'>
+            <a href='/' className=' bg-amber-100 text-sm hover:text-rose-600 hover:text-base text-center w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
       </div>
+
       
+      {/* Card Content */}
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+        
+      {/* First Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>Shop It Up</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
+
+        <div className='text-center font-medium '>
+          <p className='py-2 border-b mx-8 mt-8'>500 GB storage</p>
+           <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              Mongoose ||
+              MongoDB</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
+        </div>
+
+          <div className='flex justify-between'>
+            <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center  hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
+        
+      {/* Second Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>Techh Talk</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
+
+        <div className='text-center font-medium '>
+          <p className='py-2 border-b mx-8 mt-8'>CMS-style blog site similar to a Wordpress site where the user can post and comment.</p>
+           <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              MySQL ||
+              Sequelize||
+              Express-Handlebars ||
+            MVC paradigm</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Front End && Back End</p>
+        </div>
+
+          <div className='flex justify-between'>
+            <a href='/' className=' bg-amber-100 text-sm hover:text-rose-600 hover:text-base w-[100px] text-center rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
       
+    {/* Third Card */}
+      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+        
+        <h2 className='text-2xl font-bold text-center py-8'>Inst API</h2>
+        <img className='w-20 mx-auto' src={viper} alt="/" />
+
+        <div className='text-center font-medium '>
+          <p className='py-2 border-b mx-8 mt-8'>An API for a social network web application.</p>
+           <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
+              Javascript ||
+              Node ||
+              Express ||
+              Mongoose ||
+              MongoDB</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Back End</p>
+        </div>
+
+          <div className='flex justify-between'>
+            <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
+          <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
+          </div>
+        </div>
+      </div>      
 
 
-    </div>
   </div>
   )
 }
