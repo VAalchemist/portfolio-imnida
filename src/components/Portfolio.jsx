7a515dfce67bf5581e5045d0549fe20ee221b181
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-07-04 20:27:32 -0700

    padding margins on project photos set

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import viper from '../assets/viper.jpg';
+import duty from '../assets/duty.png';
+import forged from '../assets/forged.png';
+import poke from '../assets/pokesearch.png';
+import tech from '../assets/techh.png';
+
 
 
 const Portfolio = () => {
   return (
-    <div className='w-full h-full py-[10rem] pb-96 bg-teal-900'>
+    <div className='w-full h-full py-[5rem] pb-96 bg-teal-900'>
       
       {/* Card Content */}
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 mb-12'>
         
       {/* First Card */}
-      <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
+      <div className=' bg-teal-700 shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
-        <h2 className='text-2xl font-bold text-center py-8'>Forged Ensemble(under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <h2 className='text-2xl font-bold text-center py-4'>Forged Ensemble(under construction)</h2>
+        <img className='w-56 mx-auto rounded-lg' src={forged} alt="." />
 
         <div className=' text-center font-medium'>
-          <p className='py-2 border-b mx-8 mt-8'>Fullstack web app using MERN stack which allows the user to create music and share it amongst other exciting features.</p>
+          <p className='py-2 border-b mx-8 mt-4'>Fullstack web app using MERN stack which allows the user to create music and share it amongst other exciting features.</p>
             <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
@@ -28,10 +33,10 @@ const Portfolio = () => {
               JASON Web Token ||
               Cloudinary
             </p>
-          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Frontend & Backend</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='https://github.com/VAalchemist/forged-ensemble' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
@@ -41,10 +46,10 @@ const Portfolio = () => {
       <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
         <h2 className='text-2xl font-bold text-center py-8'>Duty Call(under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <img className='w-64 h-56 mx-auto rounded-lg' src={duty} alt="/" />
 
         <div className='text-center font-medium '>
-          <p className='py-2 border-b mx-8 mt-8'>Interactive fullstack app where user can create a profile to search, save, and set appointments available for the right handyman. </p>
+          <p className='py-2 border-b mx-8 mt-4'>Interactive fullstack app where user can create a profile to search, save, and set appointments available for the right handyman. </p>
            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
@@ -53,10 +58,10 @@ const Portfolio = () => {
               Sequelize ||
               Bcrypt ||
             EJS</p>
-          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Frontend and Deployment</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Frontend & Deployment</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
@@ -66,19 +71,19 @@ const Portfolio = () => {
       <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
         <h2 className='text-2xl font-bold text-center py-8'>KLEC Search(under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <img className='w-64 mx-auto rounded-lg' src={poke} alt="/" />
 
         <div className='text-center font-medium '>
-          <p className='py-2 border-b mx-8 mt-8'>Simple frontend focused app where use can search for pokemon and create their ideal team.</p>
+          <p className='py-2 border-b mx-8 mt-4'>Simple frontend focused app where use can search for pokemon and create their ideal team.</p>
            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
               Tailwind ||
               Html</p>
-          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Frontend</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='/' className=' bg-amber-100 text-sm hover:text-rose-600 hover:text-base text-center w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
@@ -93,10 +98,10 @@ const Portfolio = () => {
       <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
         <h2 className='text-2xl font-bold text-center py-8'>Crypto Chronometer (under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <img className='w-64 mx-auto rounded-lg' src={viper} alt="/" />
 
         <div className='text-center font-medium '>
-          <p className='py-2 border-b mx-8 mt-8'>Bitcoin tracking app where user can create profile with firebase backend authentication. </p>
+          <p className='py-2 border-b mx-8 mt-4'>Bitcoin tracking app where user can create profile with firebase backend authentication. </p>
            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
@@ -104,10 +109,10 @@ const Portfolio = () => {
               Tailwind ||
               Axios ||
             Firebase</p>
-          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Send up to 2 mollies</p>
+          <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Frontend & Backend</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center  hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
@@ -117,10 +122,10 @@ const Portfolio = () => {
       <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
         <h2 className='text-2xl font-bold text-center py-8'>Techh Talk(under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <img className='w-64 h-56 mx-auto rounded-lg' src={tech} alt="/" />
 
         <div className='text-center font-medium '>
-          <p className='py-2 border-b mx-8 mt-8'>CMS-style blog site similar to a Wordpress site where the user can post and comment.</p>
+          <p className='py-2 border-b mx-8 mt-4'>CMS-style blog site similar to a Wordpress site where the user can post and comment.</p>
            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
@@ -132,7 +137,7 @@ const Portfolio = () => {
           <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Front End && Back End</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='/' className=' bg-amber-100 text-sm hover:text-rose-600 hover:text-base w-[100px] text-center rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
@@ -142,10 +147,10 @@ const Portfolio = () => {
       <div className='bg-teal-700 w-full shadow-xl flex flex-col p-4 sm:mt-4 rounded-lg text-rose-300 hover:scale-105 duration-300' >
         
         <h2 className='text-2xl font-bold text-center py-8'>Inst API(under construction)</h2>
-        <img className='w-20 mx-auto' src={viper} alt="/" />
+        <img className='w-64 mx-auto rounded-lg' src={viper} alt="/" />
 
         <div className='text-center font-medium '>
-          <p className='py-2 border-b mx-8 mt-8'>An API for a social network web application.</p>
+          <p className='py-2 border-b mx-8 mt-4'>An API for a social network web application.</p>
            <p className='py-2 border-b mx-8'><span className='font-bold'>Tech Used: </span>
               Javascript ||
               Node ||
@@ -155,7 +160,7 @@ const Portfolio = () => {
           <p className='py-2 border-b mx-8'><span className='font-bold'>Role</span>: Back End</p>
         </div>
 
-          <div className='flex justify-between'>
+          <div className='flex justify-between mx-16'>
             <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>Code</a>
           <a href='/' className=' bg-amber-100 text-sm text-center hover:text-rose-600 hover:text-base w-[100px] rounded-md font-bold  text-teal-700 my-8 mx-auto md:mx-0 py-3 '>App</a>
           </div>
